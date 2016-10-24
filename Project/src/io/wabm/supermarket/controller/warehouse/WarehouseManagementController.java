f3662ba342570d00d4596192cff57de757c3025b
CMK <cirno.mainasuk@gmail.com>
2016-10-24 16:38:53 +0800

    chore: remove useless package

--- Project/src/io/wabm/supermarket/controller/warehouse/WarehouseManagementController.java
+++ Project/src/io/wabm/supermarket/controller/warehouse/WarehouseManagementController.java
@@ -1,16 +1,11 @@
 package io.wabm.supermarket.controller.warehouse;
 
-import io.wabm.supermarket.application.Main;
 import io.wabm.supermarket.controller.AbstractMasterDetailController;
 import io.wabm.supermarket.util.ConsoleLog;
 import io.wabm.supermarket.view.ViewPathHelper;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-
-import java.io.IOException;
 
 /**
  * Created by MainasuK on 2016-10-16.
