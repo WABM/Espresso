15cc55db3cb3a3360fb618c1d3930f7e3147bef2
CMK <cirno.mainasuk@gmail.com>
2016-10-25 16:02:51 +0800

    feat: add StageSetableController protocol

---
+++ Project/src/io/wabm/supermarket/protocol/StageSetableContoller.java
@@ -0,0 +1,10 @@
+package io.wabm.supermarket.protocol;
+
+import javafx.stage.Stage;
+
+/**
+ * Created by MainasuK on 2016-10-25.
+ */
+public interface StageSetableContoller {
+    void setStage(Stage stage);
+}
