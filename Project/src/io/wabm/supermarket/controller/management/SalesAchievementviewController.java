eb4a0ab8ea44837c903e72609f78d1c43eabd6de
MFC <804031737@qq.com>
2016-10-27 18:14:25 +0800

    新建订单 管理员系统OK。差俩图

---
+++ Project/src/io/wabm/supermarket/controller/management/SalesAchievementviewController.java
@@ -0,0 +1,44 @@
+package io.wabm.supermarket.controller.management;
+
+import io.wabm.supermarket.protocol.StageSetableContoller;
+import io.wabm.supermarket.util.ConsoleLog;
+import io.wabm.supermarket.view.ViewPathHelper;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * Created by liu on 2016-10-25 .
+ */
+public class SalesAchievementviewController {
+    @FXML Button hotgoodsButton;
+    @FXML
+    private void hotgoodsButtonPressed() {
+        ConsoleLog.print("Button pressed");
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(ViewPathHelper.class.getResource("management/Hot list.fxml"));
+            AnchorPane pane = loader.load();
+
+            Stage stage = new Stage();
+            stage.setTitle("我不知道写什么好");
+            stage.initModality(Modality.APPLICATION_MODAL);
+
+            Scene scene = new Scene(pane);
+            stage.setScene(scene);
+
+            StageSetableContoller contoller = loader.getController();
+            contoller.setStage(stage);
+
+            stage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Project/src/io/wabm/supermarket/controller/procurement/NeworderController.java
@@ -0,0 +1,28 @@
+package io.wabm.supermarket.controller.procurement;
+
+import io.wabm.supermarket.protocol.StageSetableContoller;
+import io.wabm.supermarket.util.ConsoleLog;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+/**
+ * Created by liu on 2016-10-26 .
+ */
+public class NeworderController implements StageSetableContoller {
+    @FXML Stage stage;
+    @FXML Button comfirmButton;
+    @FXML Button cancelButton;
+    @Override
+    public  void setStage(Stage stage){
+        this.stage=stage;
+    }
+    @FXML private void comfirmButtonPressed(){
+        ConsoleLog.print("Button pressed");
+        stage.close();
+    }
+    @FXML private  void setCancelButtonpressed(){
+        ConsoleLog.print("Button pressed");
+        stage.close();
+    }
+}
