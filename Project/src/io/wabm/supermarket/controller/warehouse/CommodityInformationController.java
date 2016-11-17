65ebd43fbd7bf21805557c2759657c06692dac86
CMK <cirno.mainasuk@gmail.com>
2016-11-17 12:43:16 +0800

    feat: add commodity information model

--- Project/src/io/wabm/supermarket/controller/warehouse/CommodityInformationController.java
+++ Project/src/io/wabm/supermarket/controller/warehouse/CommodityInformationController.java
@@ -1,22 +1,47 @@
 package io.wabm.supermarket.controller.warehouse;
 
 import io.wabm.supermarket.controller.SceneController;
+import io.wabm.supermarket.misc.pojo.Classification;
 import io.wabm.supermarket.misc.pojo.Commodity;
 import io.wabm.supermarket.misc.util.ConsoleLog;
+import io.wabm.supermarket.model.warehouse.CommodityInformationModel;
 import io.wabm.supermarket.view.ViewPathHelper;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.sql.Timestamp;
 
 /**
  * Created by MainasuK on 2016-11-16.
  */
 public class CommodityInformationController extends SceneController {
 
+    private CommodityInformationModel<Commodity> model;
+
     @FXML TableView<Commodity> tableView;
+    @FXML TableColumn<Commodity, String> idColumn;
+    @FXML TableColumn<Commodity, String> barCodeColumn;
+    @FXML TableColumn<Commodity, String> nameColumn;
+    @FXML TableColumn<Commodity, Integer> classificationColumn;
+    @FXML TableColumn<Commodity, String> specificationColumn;
+    @FXML TableColumn<Commodity, String> unitColumn;
+    @FXML TableColumn<Commodity, String> priceColumn;
+    @FXML TableColumn<Commodity, String> deliverySpecificationColumn;
+    @FXML TableColumn<Commodity, Timestamp> createTimestampColumn;
+
+
 
     @FXML Button backButton;
+    @FXML Button addButton;
+    @FXML Button deleteButton;
+    @FXML Button modifyButton;
+    @FXML Button searchButton;
 
     @FXML private void backButtonPressed() {
         ConsoleLog.print("button pressed");
@@ -30,6 +55,42 @@ public class CommodityInformationController extends SceneController {
     @FXML public void initialize() {
         ConsoleLog.print("CommodityInformationController init");
 
+        setupControl();
+        setupModel();
+        setupTableView();
+        setupTableViewColumn();
+
+
+
+        // Stub for develop
+        model.add(new Commodity("0", 0, "6902538006261", "脉动 青柠味", "1L", "瓶", 8.00, 12, 10*30, 10, new Timestamp(System.currentTimeMillis())));
+        model.add(new Commodity("1", 0, "6902538006261", "脉动 青柠味", "1L", "瓶", 8.99, 12, 10*30, 10, new Timestamp(System.currentTimeMillis())));
+        model.add(new Commodity("2", 0, "6902538006261", "脉动 青柠味", "1L", "瓶", 8.999, 12, 10*30, 10, new Timestamp(System.currentTimeMillis())));
+        model.add(new Commodity("3", 0, "6902538006261", "脉动 青柠味", "1L", "瓶", 8.975, 12, 10*30, 10, new Timestamp(System.currentTimeMillis())));
+    }
+
+
+    // MARK: Setup method
+
+    private void setupControl() {
+
+    }
+    private void setupModel() {
+        model = new CommodityInformationModel<>(tableView);
+    }
+    private void setupTableView() {
+
+    }
+    private void setupTableViewColumn() {
+        // Setup cell value factory
+        idColumn.setCellValueFactory(cellData -> cellData.getValue().commodityIDProperty());
+        classificationColumn.setCellValueFactory(cellData -> cellData.getValue().classificationIDProperty().asObject());
+        barCodeColumn.setCellValueFactory(cellData -> cellData.getValue().barcodeProperty());
+        nameColumn.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
+        specificationColumn.setCellValueFactory(cellData -> cellData.getValue().specificationProperty());
+        unitColumn.setCellValueFactory(cellData -> cellData.getValue().unitProperty());
+        priceColumn.setCellValueFactory(cellData -> cellData.getValue().priceProperty());
+        createTimestampColumn.setCellValueFactory(cellData -> cellData.getValue().createTimestampProperty());
     }
 
 }
--- Project/src/io/wabm/supermarket/misc/pojo/Commodity.java
+++ Project/src/io/wabm/supermarket/misc/pojo/Commodity.java
@@ -1,86 +1,217 @@
 package io.wabm.supermarket.misc.pojo;
 
+import javafx.beans.property.*;
+
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.sql.Timestamp;
+import java.text.DecimalFormat;
 
 /**
  * Created by MainasuK on 2016-11-14.
  */
 public class Commodity {
 
-    private int commodityID;
-    private int classificationID;
-    private String barcode;
-    private int shelfLife;
-    private String unit;
-    private BigDecimal price;
-    private int startStorage;
-    private int deliverySpecification;
+    private StringProperty commodityID;
+    private IntegerProperty classificationID;
+    private StringProperty barcode;
+    private StringProperty name;
+    private StringProperty specification;
+    private StringProperty unit;
+    private StringProperty price;
+    private IntegerProperty deliverySpecification;
 
-    // MARK: Setter
-    public void setCommodityID(int commodityID) {
+    private IntegerProperty shelfLife;
+    private IntegerProperty startStorage;
+
+    private ObjectProperty<Timestamp> createTimestamp;
+
+    public Commodity(String commodityID,
+                     int classificationID,
+                     String barcode,
+                     String name,
+                     String specification,
+                     String unit,
+                     double price,
+                     Integer deliverySpecification,
+                     Integer shelfLife,
+                     Integer startStorage,
+                     Timestamp createTimestamp) {
+        this.commodityID = new SimpleStringProperty(commodityID);
+        this.classificationID = new SimpleIntegerProperty(classificationID);
+        this.barcode = new SimpleStringProperty(barcode);
+        this.name = new SimpleStringProperty(name);
+        this.specification = new SimpleStringProperty(specification);
+        this.unit = new SimpleStringProperty(unit);
+        this.deliverySpecification = new SimpleIntegerProperty(deliverySpecification);
+        this.shelfLife = new SimpleIntegerProperty(shelfLife);
+        this.startStorage = new SimpleIntegerProperty(startStorage);
+        this.createTimestamp = new SimpleObjectProperty<>(createTimestamp);
+
+        BigDecimal decimal = new BigDecimal(price);
+        DecimalFormat formater = new DecimalFormat("#.00");
+        decimal.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+
+        this.price = new SimpleStringProperty(formater.format(decimal.doubleValue()));
+    }
+
+    public Commodity(StringProperty commodityID,
+                     IntegerProperty classificationID,
+                     StringProperty barcode,
+                     StringProperty name,
+                     StringProperty specification,
+                     StringProperty unit,
+                     StringProperty price,
+                     IntegerProperty deliverySpecification,
+                     IntegerProperty shelfLife,
+                     IntegerProperty startStorage,
+                     ObjectProperty<Timestamp> createTimestamp) {
         this.commodityID = commodityID;
-    }
-
-    public void setClassificationID(int classificationID) {
         this.classificationID = classificationID;
-    }
-
-    public void setBarcode(String barcode) {
         this.barcode = barcode;
-    }
-
-    public void setShelfLife(int shelfLife) {
-        this.shelfLife = shelfLife;
-    }
-
-    public void setUnit(String unit) {
+        this.name = name;
+        this.specification = specification;
         this.unit = unit;
-    }
-
-    public void setPrice(BigDecimal price) {
         this.price = price;
-    }
-
-    public void setStartStorage(int startStorage) {
-        this.startStorage = startStorage;
-    }
-
-    public void setDeliverySpecification(int deliverySpecification) {
         this.deliverySpecification = deliverySpecification;
+        this.shelfLife = shelfLife;
+        this.startStorage = startStorage;
+        this.createTimestamp = createTimestamp;
     }
 
-    // MARK: Getter
-    public int getCommodityID() {
 
+    // MARK: Setter & Getter
+
+    public String getCommodityID() {
+        return commodityID.get();
+    }
+
+    public StringProperty commodityIDProperty() {
         return commodityID;
     }
 
+    public void setCommodityID(String commodityID) {
+        this.commodityID.set(commodityID);
+    }
+
     public int getClassificationID() {
+        return classificationID.get();
+    }
+
+    public IntegerProperty classificationIDProperty() {
         return classificationID;
     }
 
+    public void setClassificationID(int classificationID) {
+        this.classificationID.set(classificationID);
+    }
+
     public String getBarcode() {
+        return barcode.get();
+    }
+
+    public StringProperty barcodeProperty() {
         return barcode;
     }
 
-    public int getShelfLife() {
-        return shelfLife;
+    public void setBarcode(String barcode) {
+        this.barcode.set(barcode);
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public String getSpecification() {
+        return specification.get();
+    }
+
+    public StringProperty specificationProperty() {
+        return specification;
+    }
+
+    public void setSpecification(String specification) {
+        this.specification.set(specification);
     }
 
     public String getUnit() {
+        return unit.get();
+    }
+
+    public StringProperty unitProperty() {
         return unit;
     }
 
-    public BigDecimal getPrice() {
+    public void setUnit(String unit) {
+        this.unit.set(unit);
+    }
+
+    public String getPrice() {
+        return price.get();
+    }
+
+    public StringProperty priceProperty() {
         return price;
     }
 
-    public int getStartStorage() {
-        return startStorage;
+    public void setPrice(String price) {
+        this.price.set(price);
     }
 
     public int getDeliverySpecification() {
+        return deliverySpecification.get();
+    }
+
+    public IntegerProperty deliverySpecificationProperty() {
         return deliverySpecification;
     }
 
+    public void setDeliverySpecification(int deliverySpecification) {
+        this.deliverySpecification.set(deliverySpecification);
+    }
+
+    public int getShelfLife() {
+        return shelfLife.get();
+    }
+
+    public IntegerProperty shelfLifeProperty() {
+        return shelfLife;
+    }
+
+    public void setShelfLife(int shelfLife) {
+        this.shelfLife.set(shelfLife);
+    }
+
+    public int getStartStorage() {
+        return startStorage.get();
+    }
+
+    public IntegerProperty startStorageProperty() {
+        return startStorage;
+    }
+
+    public void setStartStorage(int startStorage) {
+        this.startStorage.set(startStorage);
+    }
+
+    public Timestamp getCreateTimestamp() {
+        return createTimestamp.get();
+    }
+
+    public ObjectProperty<Timestamp> createTimestampProperty() {
+        return createTimestamp;
+    }
+
+    public void setCreateTimestamp(Timestamp createTimestamp) {
+        this.createTimestamp.set(createTimestamp);
+    }
 }
