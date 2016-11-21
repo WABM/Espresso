883aa70db4c2602feed3b634330172c52e3831f1
CMK <cirno.mainasuk@gmail.com>
2016-11-21 16:29:02 +0800

    feat: add MySQL database config and DBTest

--- Project/src/io/wabm/supermarket/misc/config/DBConfig.java
+++ Project/src/io/wabm/supermarket/misc/config/DBConfig.java
@@ -1,7 +1,42 @@
 package io.wabm.supermarket.misc.config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import javax.sql.DataSource;
+
 /**
  * Created by MainasuK on 2016-11-21.
  */
+@Configuration
 public class DBConfig {
+
+    @Bean
+    public BasicDataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost/Espresso?serverTimezone=CST");    // Set timezone to China Standard Time
+//        dataSource.setUrl("jdbc:mysql://192.168.1.110:3306/Espresso?serverTimezone=CST");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+        dataSource.setInitialSize(5);
+        dataSource.setMaxTotal(10);
+
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
+        return new NamedParameterJdbcTemplate(dataSource);
+    }
+
 }
--- Project/src/io/wabm/supermarket/misc/test/DBTest.java
+++ Project/src/io/wabm/supermarket/misc/test/DBTest.java
@@ -1,7 +1,59 @@
 package io.wabm.supermarket.misc.test;
 
+import static org.junit.Assert.*;
+
+import io.wabm.supermarket.misc.config.DBConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.sql.DataSource;
+
 /**
  * Created by MainasuK on 2016-11-21.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {DBConfig.class})
 public class DBTest {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    JdbcTemplate template;
+
+
+    @Test
+    public void testDataSource() {
+        assertNotNull(dataSource);
+    }
+
+    @Test
+    public void testJdbcTemplate() {
+        assertNotNull(template);
+    }
+
+
+//    @Test
+//    public void testAddSpitter() {
+//        try {
+//            repository.addSpitter(this.spitter);
+//        } catch (DataAccessException e) {
+//            assertNull(e);
+//        }
+//    }
+//
+//    @Test
+//    public void testFindSpitter() {
+//        try {
+//            Spitter spitter = repository.findOne(1);
+//            System.out.println(spitter);
+//        } catch (DataAccessException e) {
+//            assertNull(e);
+//        }
+//    }
+
 }
