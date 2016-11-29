31bbd4723faddeb2dbaddb8759d6fcd23f501594
ALS-20161114ADB\14580 <1458061487@qq.com>
2016-11-30 00:58:52 +0800

    将供应商基本信息表显示在界面上

--- Project/src/io/wabm/supermarket/controller/procurement/CommoditySupplierManagementViewController.java
+++ Project/src/io/wabm/supermarket/controller/procurement/CommoditySupplierManagementViewController.java
@@ -1,12 +1,17 @@
 package io.wabm.supermarket.controller.procurement;
 
-import io.wabm.supermarket.protocol.StageSetableController;
+import io.wabm.supermarket.controller.SceneController;
+import io.wabm.supermarket.misc.pojo.Supplier;
 import io.wabm.supermarket.misc.util.ConsoleLog;
+import io.wabm.supermarket.model.procurement.CommoditySupplierModel;
+import io.wabm.supermarket.protocol.StageSetableController;
 import io.wabm.supermarket.view.ViewPathHelper;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -15,10 +20,56 @@ import java.io.IOException;
 /**
  * Created by 14580 on 2016/11/20 0020.
  */
-public class CommoditySupplierManagementViewController {
+public class CommoditySupplierManagementViewController extends SceneController {
+
+    private CommoditySupplierModel<Supplier> model;
+
+    @FXML TableView<Supplier> tableView;
+
+    @FXML TableColumn<Supplier, Integer> idColumn;
+    @FXML TableColumn<Supplier, String> nameColumn;
+    @FXML TableColumn<Supplier, String> representativeColumn;
+    @FXML TableColumn<Supplier, String> phoneColumn;
+    @FXML TableColumn<Supplier, String> addressColumn;
+
+
     @FXML Button newButton;
     @FXML Button deleteButton;
     @FXML Button modifyButton;
+
+    @FXML public void initialize() {
+        ConsoleLog.print("CommoditySupplierManagementViewController init");
+
+        setupControl();
+        setupModel();
+        setupTableView();
+        setupTableViewColumn();
+
+    }
+    private void setupTableViewColumn() {
+        idColumn.setCellValueFactory(cellData -> cellData.getValue().supplierIDProperty().asObject());
+        nameColumn.setCellValueFactory(cellData -> cellData.getValue().supplierNameProperty());
+        representativeColumn.setCellValueFactory(cellData -> cellData.getValue().linkmanProperty());
+
+        phoneColumn.setCellValueFactory(cellData -> cellData.getValue().phoneProperty());
+        addressColumn.setCellValueFactory(cellData -> cellData.getValue().addressProperty());
+    }
+
+    private void setupModel() {
+        model = new CommoditySupplierModel<>(tableView);
+        model.fetchData(isSuccess -> {
+            ConsoleLog.print("Fetch is " + (isSuccess ? "success" : "failed"));
+            return null;
+        });
+    }
+
+    private void setupTableView() {
+    }
+
+    private void setupControl() {
+    }
+
+
     @FXML private void newButtonPressed(){
         ConsoleLog.print("Button pressed");
         try{
--- Project/src/io/wabm/supermarket/model/procurement/CommoditySupplierModel.java
+++ Project/src/io/wabm/supermarket/model/procurement/CommoditySupplierModel.java
@@ -1,30 +1,74 @@
 package io.wabm.supermarket.model.procurement;
 
+import io.wabm.supermarket.misc.config.DBConfig;
+import io.wabm.supermarket.misc.pojo.Classification;
+import io.wabm.supermarket.misc.pojo.Supplier;
 import io.wabm.supermarket.misc.util.ConsoleLog;
-import io.wabm.supermarket.model.Model;
-import javafx.collections.FXCollections;
+import io.wabm.supermarket.misc.util.WABMThread;
+import io.wabm.supermarket.model.TableViewModel;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
 import javafx.scene.control.TableView;
+import javafx.util.Callback;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.stereotype.Repository;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.util.Assert;
+
+import java.sql.ResultSet;
+import java.util.List;
 
 /**
  * Created by liu on 2016-11-21 .
  */
-public class  CommoditySupplierModel<T> extends Model<T> {
+public class  CommoditySupplierModel<T> extends TableViewModel<T> {
+    private final String kSelectAll = "SELECT f.* FROM wabm.supplier f";
+    private Service<Void> backgroundThread;
+    public CommoditySupplierModel(TableView<T> tableView) {
 
-            private TableView<T> tableView;
+        super(tableView);
+        ConsoleLog.print("CommoditySupplierModel init");
+    }
 
-            public CommoditySupplierModel(TableView tableView) {
-                ConsoleLog.print("CommoditySupplierModel init");
+    public void fetchData(Callback<Boolean, Void> callback) {
+        ConsoleLog.print("fetching data…");
+        Assert.notNull(jdbcOperations);
 
-                this.tableView = tableView;
+        new WABMThread().run((_void) -> {
 
-                list = FXCollections.observableArrayList();
-                tableView.setItems(list);
+            try{
+            List<Supplier> templist = jdbcOperations.query(kSelectAll,
+                    (ResultSet resultSet, int i) -> {
+                        Supplier supplier = new Supplier(
+                                resultSet.getInt("supplier_id"),
+                                resultSet.getString("name"),
+                                resultSet.getString("representative_name"),//date类型的，要转换为string类
+                                resultSet.getString("phone"),
+                                resultSet.getString("address")
+                        );
+
+                        return supplier;
+                    }
+            );
+
+            list.clear();
+            list.addAll((T[]) templist.toArray());
+
+            // Return callback with isfetchSuccess flag;
+            // TODO:
+            callback.call(true);
+
+            } catch (DataAccessException exception) {
+                callback.call(false);
+                exception.printStackTrace();
             }
+            return null;
+        });
 
-            // MARK: DataSource protocol
-            public void add(T item) {
-                list.add(item);
-            }
+    }  // end of fetchData
+
 }
 
 
