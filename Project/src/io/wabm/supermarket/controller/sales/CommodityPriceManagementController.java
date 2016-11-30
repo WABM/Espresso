a1fc4e6f994a9c200652407ec85696016118e69d
1024909687@qq.com <1024909687@qq.com>
2016-11-30 10:52:59 +0800

    销售页面数据填充基本完成

--- Project/src/io/wabm/supermarket/controller/sales/CommodityPriceManagementController.java
+++ Project/src/io/wabm/supermarket/controller/sales/CommodityPriceManagementController.java
@@ -1,5 +1,8 @@
 package io.wabm.supermarket.controller.sales;
 
+import io.wabm.supermarket.controller.SceneController;
+import io.wabm.supermarket.misc.pojo.Classification;
+import io.wabm.supermarket.model.warehouse.CommodityClassificationInformationModel;
 import io.wabm.supermarket.protocol.StageSetableContoller;
 import io.wabm.supermarket.misc.util.ConsoleLog;
 import io.wabm.supermarket.view.ViewPathHelper;
@@ -7,6 +10,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -16,7 +23,45 @@ import java.io.IOException;
 /**
  * Created by MR-lulu on 2016/10/27 0027.
  */
-public class CommodityPriceManagementController{
+public class CommodityPriceManagementController extends SceneController {
+
+    private CommodityClassificationInformationModel<Classification> Classmodel;
+
+    @FXML private TableView<Classification> tableView;
+
+    @FXML private TableColumn<Classification, String> nameColumn;
+    @FXML private TableColumn<Classification,Double> profitRate;
+    @FXML private TableColumn<Classification,Double> taxRate;
+    @FXML private TableColumn<Classification, Hyperlink> actionColumn;
+
+    @FXML public void initialize() {
+        ConsoleLog.print("CommodityPriceManagementController init");
+
+        setupControl();
+        setupModel();
+        setupTableView();
+        setupTableViewColumn();
+    }
+
+    private void setupControl(){
+
+    }
+    private void setupModel(){
+        Classmodel = new CommodityClassificationInformationModel<>(tableView);
+
+        // TODO: loading info need add to view (Spinner or just Loading… (e.g. 加载中……)
+        Classmodel.fetchData(isSuccess -> {
+            ConsoleLog.print("Fetch is " + (isSuccess ? "success" : "failed"));
+            return null;
+        });
+    }
+    private void setupTableView(){}
+    private void setupTableViewColumn(){
+        nameColumn.setCellValueFactory(new PropertyValueFactory<Classification, String>("name"));
+        profitRate.setCellValueFactory(new PropertyValueFactory<Classification, Double>("profitRate"));
+        taxRate.setCellValueFactory(new PropertyValueFactory<Classification, Double>("taxRate"));
+    }
+    /*
     @FXML Button modify;
 
     @FXML private void modifyButtonPressed(){
@@ -42,4 +87,5 @@ public class CommodityPriceManagementController{
             e.printStackTrace();
         }
     }
+    */
 }
---
+++ Project/src/io/wabm/supermarket/controller/sales/TransactionRecordDetailController.java
@@ -0,0 +1,79 @@
+package io.wabm.supermarket.controller.sales;
+
+import io.wabm.supermarket.controller.SceneController;
+import io.wabm.supermarket.misc.pojo.TransactionRecordDetail;
+import io.wabm.supermarket.misc.util.ConsoleLog;
+import io.wabm.supermarket.model.sales.TransationRecordDetailModel;
+import io.wabm.supermarket.view.ViewPathHelper;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+/**
+ * Created by Administrator on 2016/11/28 0028.
+ */
+public class TransactionRecordDetailController extends SceneController {
+
+    private TransationRecordDetailModel<TransactionRecordDetail> model;
+    @FXML Button backButton;
+
+    @FXML private TableView<TransactionRecordDetail> tableView;
+    @FXML private TableColumn<TransactionRecordDetail,String> recordID;
+    @FXML private TableColumn<TransactionRecordDetail,String> commodityID;
+    @FXML private TableColumn<TransactionRecordDetail,String> barcode;
+    @FXML private TableColumn<TransactionRecordDetail,String> name;
+    @FXML private TableColumn<TransactionRecordDetail,Integer> classificationID;
+    @FXML private TableColumn<TransactionRecordDetail,String> specification;
+    @FXML private TableColumn<TransactionRecordDetail,String> unit;
+    @FXML private TableColumn<TransactionRecordDetail,Integer> quantity;
+    @FXML private TableColumn<TransactionRecordDetail,Double> price;
+
+
+    @FXML public void initialize() {
+        ConsoleLog.print("TransactionRecordController init");
+
+        setupControl();
+        setupModel();
+        setupTableView();
+        setupTableViewColumn();
+    }
+    @FXML private void backButtonPressed() {
+        ConsoleLog.print("button pressed");
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(ViewPathHelper.class.getResource("sales/TransactionRecordsView.fxml"));
+
+        navigationTo(loader);
+    }
+    private void setupControl(){
+    }
+    private void setupModel(){
+        model = new TransationRecordDetailModel<>(tableView);
+    }
+    private void setupTableView(){}
+    private void setupTableViewColumn(){
+        recordID.setCellValueFactory(new PropertyValueFactory<TransactionRecordDetail, String>("recordID"));
+        commodityID.setCellValueFactory(new PropertyValueFactory<TransactionRecordDetail, String>("commodityID"));
+        barcode.setCellValueFactory(new PropertyValueFactory<TransactionRecordDetail, String>("barcode"));
+        name.setCellValueFactory(new PropertyValueFactory<TransactionRecordDetail, String>("name"));
+        classificationID.setCellValueFactory(new PropertyValueFactory<TransactionRecordDetail, Integer>("classificationID"));
+        specification.setCellValueFactory(new PropertyValueFactory<TransactionRecordDetail, String>("specification"));
+        unit.setCellValueFactory(new PropertyValueFactory<TransactionRecordDetail, String>("unit"));
+        quantity.setCellValueFactory(new PropertyValueFactory<TransactionRecordDetail, Integer>("quantity"));
+        price.setCellValueFactory(new PropertyValueFactory<TransactionRecordDetail, Double>("price"));
+    }
+
+    public void fetchWith(int recordID) {
+
+        this.model.fetchData(recordID,
+                (isSuccess) -> {
+                    ConsoleLog.print("Fetch is " + (isSuccess ? "success" : "failed"));
+                    return null;
+                }
+        );
+
+    }
+}
